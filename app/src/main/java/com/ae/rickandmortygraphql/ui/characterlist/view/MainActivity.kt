bef83388115430ef1b1c9d96bae1e31307a3ad0e
Andrés Rivas <andreselias2303@gmail.com>
2021-10-10 10:46:25 -0300

    Refactored packages order

--- app/src/main/java/com/ae/rickandmortygraphql/ui/characterlist/view/MainActivity.kt
+++ app/src/main/java/com/ae/rickandmortygraphql/ui/characterlist/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ae.rickandmortygraphql
+package com.ae.rickandmortygraphql.ui.characterlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ae.rickandmortygraphql.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
