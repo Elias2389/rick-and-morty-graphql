8fd6c3414ee3958a1ffb7b7f1e8e3a707ddde363
Andrés Rivas <andreselias2303@gmail.com>
2021-10-10 10:55:16 -0300

    Added viewBinding

--- app/src/main/java/com/ae/rickandmortygraphql/ui/characterlist/view/MainActivity.kt
+++ app/src/main/java/com/ae/rickandmortygraphql/ui/characterlist/view/MainActivity.kt
@@ -3,10 +3,16 @@ package com.ae.rickandmortygraphql.ui.characterlist.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ae.rickandmortygraphql.R
+import com.ae.rickandmortygraphql.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
